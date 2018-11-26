896677db781abcbd5e03f53579c96d2911ad3866
Filip Kernan <filip.kernan@gmail.com>
2018-11-26 18:43:44 -0500

    checks if a bst is a heap

--- src/HeapChecker.java
+++ src/HeapChecker.java
@@ -15,8 +15,15 @@ public class HeapChecker {
 
 
     boolean isHeap(IBinTree tree){
-        if(tree.size() == 0){
-
+        if(tree.getRight() == null && tree.getLeft() == null){
+            return true;
+        }else if(tree.getLeft() == null && tree.getRight() != null){
+            return tree.getRight().getData() > tree.getData() && isHeap(tree.getRight());
+        }else if(tree.getRight() == null && tree.getLeft() != null){
+            return tree.getLeft().getData() > tree.getData() && isHeap(tree.getLeft());
+        }else{
+            return tree.getLeft().getData() > tree.getData() && tree.getRight().getData() > tree.getData() &&
+                    isHeap(tree.getLeft())  && isHeap(tree.getRight());
         }
 
     }
