bc029a8f4cca44ea4ab056391974738850360b9c
Unknown <pilotpatrick2000@gmail.com>
2018-11-27 20:32:34 -0500

    YAY

--- src/HeapChecker.java
+++ src/HeapChecker.java
@@ -41,4 +41,24 @@ public class HeapChecker {
             return soFar;
         }
     }
+
+    boolean heapsTheSame(LinkedList<Integer> first, LinkedList<Integer> second, boolean addOrRemove) {              // ADD IS TRUE REMOVE IS FALSE
+        if (first.size() != second.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < first.size(); i++) {
+                for (int j = 0; j < second.size(); j++) {
+                    if (first.get(i) == second.get(j)) {
+                        first.remove(i);
+                        second.remove(j);
+                    }
+                }
+            }
+            if (addOrRemove) {
+                return (first.size() == 0 && second.size() == 1);
+            } else {
+                return (first.size() == 1 && second.size() == 0);
+            }
+        }
+    }
 }
